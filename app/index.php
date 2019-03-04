0564f0158fc46f4200a636e68e6faa022a070611
Alexander <dev@ladygin.me>
2019-03-04 19:34:17 +0200

    Last edits

--- app/index.php
+++ app/index.php
@@ -1,5 +1,22 @@
 <?php
   require_once 'connect.php';
+
+  function createRandomVal($val) {
+      $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      srand((double)microtime() * 1000000);
+      $i = 0;
+      $pass = '';
+      while ($i < $val) {
+          $num = rand() % 64;
+          $tmp = substr($chars, $num, 1);
+          $pass = $pass . $tmp;
+          $i++;
+      }
+      return $pass;
+  }
+  $date = new DateTime();
   
-  echo $twig->render('site/index.twig');
+  echo $twig->render('site/index.twig', [ 'sid' => createRandomVal(32) ]);
+
+  // echo $twig->render('site/index.twig');
 ?>
